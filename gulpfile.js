b4d9e73899b3df07e46fe5f92959b6a78156e65d
Jeremy Samuel <jeremysamuel@Jeremys-iMac.local>
2018-07-14 19:18:08 -0400

    Revert "gulp and node setup"
    This reverts commit 2bfb7cabe9e2eeade5fb01e8bfb0a8f41da5558d.

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-//compile sass
-gulp.task('sass', function() {
-    return gulp.src('wp-content/themes/bubbo-theme/src/scss/site.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('wp-content/themes/bubbo-theme/dist/css/'))
-});
