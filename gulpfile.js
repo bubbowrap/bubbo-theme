353c7d5c36d1bda2e511f9a3fbf8e1e1e0c6ca36
Jeremy Samuel <jeremysamuel@Jeremys-iMac.local>
2018-08-14 21:05:06 -0400

    file setup.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 //compile sass
 gulp.task('sass', function() {
-    return gulp.src('wp-content/themes/bubbo-theme/src/scss/site.scss')
+    return gulp.src('wp-content/themes/bubbo-theme/src/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('wp-content/themes/bubbo-theme/dist/css/'))
 });
