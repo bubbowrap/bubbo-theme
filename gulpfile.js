9365e70bfcadff34fd34b7195455d07415da3e45
Jeremy Samuel <jeremysamuel@Jeremys-iMac.local>
2018-10-13 16:45:37 -0400

    adding package json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify');
+    
 //compile sass
 gulp.task('sass', function() {
     return gulp.src('wp-content/themes/bubbo-theme/src/scss/**/*.scss')
@@ -11,4 +14,29 @@ gulp.task('sass', function() {
 //compile on save
 gulp.task('watch', function() {
     gulp.watch('wp-content/themes/bubbo-theme/src/scss/**/*.scss', ['sass']);
+});
+
+
+//minifies js
+gulp.task('minify-js', function() {
+   return gulp.src('wp-content/themes/bubbo-theme/src/js/**/*.js')
+   .pipe(uglify())
+   .pipe(gulp.dest('wp-content/themes/bubbo-theme/dist/js/'))
+});
+
+//minifies css
+gulp.task('minify-css', function() {
+    return gulp.src('wp-content/themes/bubbo-theme/src/scss/**/*.scss')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('wp-content/themes/bubbo-theme/dist/css/'))
+});
+
+//builds everything
+gulp.task('build', ['minify-css', 'minify-js'], function() {
+    console.log('Building and...all done!');
 });
