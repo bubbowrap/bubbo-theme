8976d6c652d10ba62c86b910d8a32259730716f3
Jeremy Samuel <pentium6.0@gmail.com>
2019-11-29 17:46:06 -0500

    adding blog section/minor refactoring

--- wp-content/themes/bubbo-theme/index.php
+++ wp-content/themes/bubbo-theme/index.php
@@ -11,10 +11,39 @@ if ( ! class_exists( 'Timber' ) ) {
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 
+$posts_displayed = get_option('posts_per_page');
+$query_args = array(
+    'post_type' => 'post',
+    'posts_per_page' => $posts_displayed,
+    'paged' => $paged
+);
+
+$context['title'] = 'Blog';
+
+if ( is_day() ) {
+	$context['title'] = 'Archive: '.get_the_date( 'D M Y' );
+} else if ( is_month() ) {
+	$context['title'] = 'Archive: '.get_the_date( 'M Y' );
+} else if ( is_year() ) {
+	$context['title'] = 'Archive: '.get_the_date( 'Y' );
+} else if ( is_tag() ) {
+	$context['title'] = single_tag_title( '', false );
+} else if ( is_category() ) {
+	$context['title'] = single_cat_title( '', false );
+	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
+} else if ( is_post_type_archive() ) {
+	$context['title'] = post_type_archive_title( '', false );
+	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
+}
+
+// Get List of Categories
+$context['categories'] = Timber::get_terms('category');
+
 $templates = array( 'index.twig' );
 if ( is_home() ) {
 	
 	//add homepage file if home
-	array_unshift( $templates, 'front-page.twig' );
+	array_unshift( $templates, 'archive.twig' );
 }
+
 Timber::render( $templates, $context );
